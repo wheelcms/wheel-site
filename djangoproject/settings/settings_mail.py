68c38a13de93906886e56617e14ab4468fd6b441
Ivo van der Wijk <github@in.m3r.nl>
2012-09-27 11:41:34 +0200

    damn

--- djangoproject/settings/settings_mail.py
+++ djangoproject/settings/settings_mail.py
@@ -1,5 +1,5 @@
-MAIL_SENDER = 'stracks@in.m3r.nl'
-POSTMARK_API_KEY = '58a00b52-5790-489a-ae46-7ef7f6e79674'
-POSTMARK_SENDER = 'stracks@in.m3r.nl'
+MAIL_SENDER = 'sender@example.com'
+POSTMARK_API_KEY = 'xxxx'
+POSTMARK_SENDER = 'xxx@xxxx'
 EMAIL_BACKEND = 'postmark.django_backend.EmailBackend'
 
