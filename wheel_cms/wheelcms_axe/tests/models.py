f9ae4ad21563e026e0cab32e54c556688f21ab55
Ivo van der Wijk <github@in.m3r.nl>
2012-12-29 22:48:30 +0100

    first few handler tests

--- wheel_cms/wheelcms_axe/tests/models.py
+++ wheel_cms/wheelcms_axe/tests/models.py
@@ -1,4 +1,5 @@
-from wheelcms_axe.models import Node, Content
+from wheelcms_axe.models import Content
+from wheelcms_spokes.models import formfactory
 
 class Type1(Content):
     pass
@@ -7,3 +8,7 @@ class Type2(Content):
     pass
 
 
+from wheelcms_axe.models import type_registry
+
+type_registry.register("type1", Type1, formfactory(Type1))
+type_registry.register("type2", Type1, formfactory(Type2))
---
+++ wheel_cms/wheelcms_axe/tests/test_handler.py
@@ -0,0 +1,53 @@
+from wheelcms_axe.main import MainHandler
+from wheelcms_axe.models import Node
+from wheelcms_axe.tests.models import Type1
+
+from twotest.util import create_request
+
+class MainHandlerTestable(MainHandler):
+    """ intercept template() call to avoid rendering """
+    def template(self, path):
+        return dict(path=path, context=self.context)
+
+
+class TestMainHandler(object):
+    def test_coerce_instance(self, client):
+        """ coerce a dict holding an instance path """
+        root = Node.root()
+        a = root.add("a")
+        res = MainHandler.coerce(dict(instance="a"))
+        assert 'instance' in res
+        assert res['instance'] == a
+        assert 'parent' not in res
+
+    def test_coerce_parent(self, client):
+        """ coerce a dict holding an parent path """
+        root = Node.root()
+        a = root.add("a")
+        res = MainHandler.coerce(dict(parent="a"))
+        assert 'parent' in res
+        assert res['parent'] == a
+        assert 'instance' not in res
+
+    def test_coerce_instance_parent(self, client):
+        """ coerce a dict holding both instance and parent """
+        root = Node.root()
+        a = root.add("a")
+        b = a.add("b")
+        res = MainHandler.coerce(dict(instance="b", parent="a"))
+        assert 'instance' in res
+        assert 'parent' in res
+        assert res['instance'] == b
+        assert res['parent'] == a
+
+    def test_create_get_root(self, client):
+        """ test create on root - get """
+        root = Node.root()
+        Type1(node=root).save()
+        request = create_request("GET", "/", data=dict(type="type1"))
+        handler = MainHandlerTestable(request=request, instance=root)
+        # import pytest; pytest.set_trace()
+        create = handler.create()
+        assert create['path'] == "wheelcms_axe/create.html"
+        assert 'form' in create['context']
+        
