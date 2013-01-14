2d511b2570baed2d379ede3bcfc95dcbb4aa9b89
Ivo van der Wijk <github@in.m3r.nl>
2013-01-14 15:22:30 +0100

    forgotten spoke tests

---
+++ wheel_cms/wheelcms_spokes/tests/test_news.py
@@ -0,0 +1,20 @@
+"""
+    type specific test based on spoke base tests
+"""
+from wheelcms_spokes.tests.test_spoke import BaseSpokeTest, BaseSpokeTemplateTest
+from wheelcms_spokes.models import NewsType
+
+
+class TestNewsSpokeTemplate(BaseSpokeTemplateTest):
+    """ Test the News type """
+    type = NewsType
+    typename = "news"
+
+    def valid_data(self):
+        """ return additional data for News validation """
+        return dict(intro="Hi There!", body="Hello World")
+
+class TestNewsSpoke(BaseSpokeTest):
+    """ Test the News type """
+    type = NewsType
+    typename = "news"
---
+++ wheel_cms/wheelcms_spokes/tests/test_page.py
@@ -0,0 +1,21 @@
+"""
+    type specific test based on spoke base tests
+"""
+from wheelcms_spokes.tests.test_spoke import BaseSpokeTest, BaseSpokeTemplateTest
+from wheelcms_spokes.models import PageType
+
+
+class TestPageSpokeTemplate(BaseSpokeTemplateTest):
+    """ Test the Page type """
+    type = PageType
+    typename = "page"
+
+    def valid_data(self):
+        """ return additional data for Page validation """
+        return dict(body="Hello World")
+
+
+class TestPageSpoke(BaseSpokeTest):
+    """ Test the Page type """
+    type = PageType
+    typename = "page"
