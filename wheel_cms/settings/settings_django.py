e56e1b05e1390eaf397c00de49b3e23afa62bba3
Ivo van der Wijk <github@in.m3r.nl>
2013-01-24 16:40:40 +0100

    setup context processors; specifically the configuration one

--- wheel_cms/settings/settings_django.py
+++ wheel_cms/settings/settings_django.py
@@ -20,6 +20,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+
+    "wheelcms_axle.context_processors.configuration",
+)
+
 ROOT_URLCONF = 'urls'
 
 INSTALLED_APPS = (
