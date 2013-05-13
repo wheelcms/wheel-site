5123898b62361426a62039eea360e8e2821b6c35
Ivo van der Wijk <github@in.m3r.nl>
2013-05-13 11:04:18 +0200

    copied readme from old wheel-cms project

--- wheel_cms/settings/local.py
+++ wheel_cms/settings/local.py
@@ -5,6 +5,8 @@ from wheelcms_project.settings.base.util import get_env_variable
 SITE_ID = 1
 # local mail config
 
+ADMINS = ( ( "Ivo", "test-wheel@in.m3r.nl"),  )
+
 DEBUG = True
 
 SQLITE_DEFAULT_DB = {
@@ -25,6 +27,7 @@ DATABASES = {
     'default': SQLITE_DEFAULT_DB,
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # INSTALLED_APPS += ('debug_toolbar', )
 # INTERNAL_IPS = ('127.0.0.1',)
