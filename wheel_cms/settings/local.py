441c6e1a1229a814b46999408327cbe05c5c8298
Ivo van der Wijk <github@in.m3r.nl>
2013-05-17 15:25:59 +0200

    use/support url based db config

--- wheel_cms/settings/local.py
+++ wheel_cms/settings/local.py
@@ -9,23 +9,6 @@ ADMINS = ( ( "Ivo", "test-wheel@in.m3r.nl"),  )
 
 DEBUG = True
 
-SQLITE_DEFAULT_DB = {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'wheel.db',
-}
-
-PG_DEFAULT_DB = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'somename',
-        'USER': 'someone',  # get_env_variable ...
-        'PASSWORD': 'somesecret',  # get_env_variable ...
-        'HOST': 'localhost',
-        'PORT': '',
-}
-
-DATABASES = {
-    'default': SQLITE_DEFAULT_DB,
-}
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
