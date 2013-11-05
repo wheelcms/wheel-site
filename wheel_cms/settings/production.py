311e02e13bf7ffd9f138fb562b02d51283e89abd
Ivo van der Wijk <github@in.m3r.nl>
2013-11-05 13:39:46 +0100

    Remove old style db config

--- wheel_cms/settings/production.py
+++ wheel_cms/settings/production.py
@@ -1,20 +1,6 @@
 from settings.base import *
 from wheelcms_project.settings.base.util import get_env_variable
 
-if not DATABASE_URL:
-    PG_DEFAULT_DB = {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': get_env_variable('DB_NAME'),
-            'USER': get_env_variable('DB_USER'),
-            'PASSWORD': get_env_variable('DB_PASSWORD'),
-            'HOST': get_env_variable('DB_HOST', 'localhost'),
-            'PORT': get_env_variable('DB_PORT', ''),
-    }
-
-    DATABASES = {
-            'default': PG_DEFAULT_DB
-    }
-
 DEBUG=False
 
 STRACKS_URL = get_env_variable('STRACKS_URL', '')
